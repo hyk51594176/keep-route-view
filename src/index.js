8211a778c8b42571baf6cd16c8ed0079ed752206
hanyukai <yukai.han@st-creditech.com>
2019-01-21 10:34:59 +0800

    优化

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import KeepRouteView from './keepRouteView'
-export default function install (Vue) {
-  if (install.Installed) return
-  Vue.component(KeepRouteView.name, KeepRouteView)
-  install.Installed = true
-}
